24b22ab254847c0bbe4b94bb7792bbcbb5e70c66
mohi14 <mohiuddinkhan1409@gmail.com>
2023-05-04 16:42:42 +0600

    search section done

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -4,13 +4,13 @@ const List = () => {
   return (
     <section className="container mx-auto">
       <h3 className="text-center text-[1.5rem] mb-16">
-        Great! We Will Need Some Information:
+        Please Tell Us About Your Storage Place:
       </h3>
-      <div className="grid grid-cols-2 gap-20">
+      <form className="grid grid-cols-2 gap-20">
         <div className="flex items-center">
-          <form className="w-[300px] flex flex-col gap-5">
+          <div className="w-[350px] flex flex-col gap-5">
             <div className="flex justify-between">
-              <label>Full Name:</label>
+              <label>Storage Name:</label>
               <input
                 className="inputFiled"
                 type="text"
@@ -19,7 +19,7 @@ const List = () => {
               />
             </div>
             <div className="flex justify-between">
-              <label>Id Number:</label>
+              <label>Available Space:</label>
               <input
                 className="inputFiled"
                 type="number"
@@ -28,7 +28,7 @@ const List = () => {
               />
             </div>
             <div className="flex justify-between">
-              <label>Email:</label>
+              <label>Address:</label>
               <input
                 className="inputFiled"
                 type="email"
@@ -37,11 +37,11 @@ const List = () => {
               />
             </div>
             <div className="flex justify-between">
-              <label>Phone:</label>
+              <label>Price:</label>
               <input className="inputFiled" type="tel" name="phone" required />
             </div>
             <div className="flex justify-between">
-              <label>Password:</label>
+              <label>Category:</label>
               <input
                 className="inputFiled"
                 type="password"
@@ -52,16 +52,48 @@ const List = () => {
             <div className="text-white text-end mr-10">
               <button className="border-2 px-2">Sign Me Up!</button>
             </div>
-          </form>
+          </div>
         </div>
-        <div className="image-reflection h-[350px]">
-          <img
-            className="rounded-[20px] h-full"
-            src="https://media.istockphoto.com/id/157696118/tr/foto%C4%9Fraf/american-flag-with-uncle-sam-pointing-at-you.jpg?s=612x612&w=is&k=20&c=EkdV1C70aDHQMmApFuFBXKKqaYNuu9sRyfhEhaf7Fgw="
-            alt=""
-          />
+        <div className="flex flex-col gap-5">
+          <div className="flex gap-10">
+            <label>Description:</label>
+            <textarea className="inputFiled w-1/2" />
+          </div>
+          <div className="flex items-center gap-5">
+            <p>Dates: </p>
+            <div className="flex gap-3 items-center">
+              <label>From:</label>
+              <input
+                className="inputFiled"
+                type="password"
+                name="password"
+                required
+              />
+            </div>
+            <div className="flex gap-3 items-center">
+              <label>To:</label>
+              <input
+                className="inputFiled"
+                type="password"
+                name="password"
+                required
+              />
+            </div>
+          </div>
+          <div className="flex gap-10">
+            <label>Add Photo:</label>
+            <span
+              style={{ lineHeight: "16px" }}
+              className="cursor-pointer rounded-[5px] bg-[#2e2e2e] border text-[20px] text-white  px-2"
+            >
+              +
+            </span>
+          </div>
+          <div className="text-white text-center">
+            <button className="border-2 px-8">List it!</button>
+          </div>
         </div>
-      </div>
+      </form>
     </section>
   );
 };
